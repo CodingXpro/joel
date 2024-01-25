94e4b0b23a6b7906cb7e2816c0bda8392f3ecc61
unknown <krishnassm420012@gmail.com>
2024-01-25 11:44:45 +0530

    hello

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import authRoute from './routes/auth.js'
+
+import hotelRoute from './routes/hotels.js'
+
+import usersRoute from './routes/users.js'
+
+import roomsRoute from './routes/rooms.js'
+import cookieParser from 'cookie-parser';
+
+
+const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+const connect=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("connected to mongodb")
+    } catch (error) {
+        throw error
+    }
+}
+
+app.use('/auth',authRoute);
+app.use('/hotels',hotelRoute);
+app.use('/rooms',roomsRoute);
+app.use('/users',usersRoute);
+
+
+
+
+app.listen(8800,()=>{
+    connect();
+    console.log("Connested to backend")
+})
